400017634c8d904016ac614d77ab9ee932807e3b
Asger Hallas <ahl@asgerhallas.dk>
2015-04-23 08:12:27 +0200

    test project

---
+++ Nontainer.Tests/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Nontainer.Mvc;
+using Xunit;
+
+namespace Nontainer.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void FactMethodName()
+        {
+            
+        }
+
+        public class CR : CompositionRoot
+        {
+            public CR()
+            {
+            }
+        }
+    }
+}
